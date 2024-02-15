24cf4fb96162de71948e49c7a1f24a283a3fc55e
abdelrahman-khaled-nvtc <118051713+abdelrahman-khaled-nvtc@users.noreply.github.com>
2024-02-15 09:56:01 +0000

    Adding Student Card

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# blog-project-11-sec
+This project is a part of WEB202 Course.
---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header pt-10 gap-y-10">
+        <Card/>
+        <Card/>
+        <Card/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
